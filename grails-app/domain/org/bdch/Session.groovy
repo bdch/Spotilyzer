a2cac8291582b6e003afbfc5f6323b407b44ecd6
bdch <de.zac007@gmail.com>
2025-07-31 21:11:50 +0200

    - added sesssion control

--- grails-app/domain/org/bdch/Session.groovy
+++ grails-app/domain/org/bdch/Session.groovy
@@ -2,24 +2,27 @@ package org.bdch
 
 class Session {
 
-   Long id
-   Long version
-   String sessionKey
-   User owner
-   Long cts // creation timestamp
+    Long id
+    Long version
+    String sessionKey
+    User owner
+    Long cts // Two timestamps I guess
+    Long user_id
+    Long creation_timestamp
 
-   static belongsTo = [owner: User]
+    static belongsTo = [owner: User]
 
-   static mapping = {
-      table 'user_session'
-      sessionKey column: 'session_key', unique: true
-      owner column: 'owner'
-      cts column: 'creation_timestamp'
-   }
+    static mapping = {
+        table 'user_session'
+        sessionKey column: 'session_key', unique: true
+        owner column: 'owner'
+        cts column: 'cts'
+        user_id column: 'user_id'
+        creation_timestamp column: 'creation_timestamp'
+    }
 
-   static constraints = {
-      sessionKey nullable: false, blank: false, unique: true
-      owner nullable: false
-      cts nullable: false
+    static constraints = {
+        sessionKey nullable: false, blank: false, unique: true
+        owner nullable: false
     }
 }
