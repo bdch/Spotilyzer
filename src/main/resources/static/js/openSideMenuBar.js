dbe8aaebc3b16ffe10b511f3c287de680e6c1837
bdch <de.zac007@gmail.com>
2025-08-03 15:41:29 +0200

    - added sidebar menu

---
+++ src/main/resources/static/js/openSideMenuBar.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("main-sidebarMenu").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("main-sidebarMenu").style.width = "0";
+}
