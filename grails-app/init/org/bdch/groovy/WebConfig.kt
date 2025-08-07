c1580334b0f25480e9c4699362b17a6838e8f5f1
bdch <de.zac007@gmail.com>
2025-08-07 09:25:27 +0200

    - Working stable compillable version

--- grails-app/init/org/bdch/groovy/WebConfig.kt
+++ grails-app/init/org/bdch/groovy/WebConfig.kt
@@ -1,16 +1,9 @@
-//package org.bdch.groovy
-//
-//import org.springframework.context.annotation.Bean
-//import org.springframework.context.annotation.Configuration
-//import org.springframework.web.servlet.ViewResolver
-//
-//@Configuration
-//class WebConfig {
-//
-//    @Bean
-//    fun webConfig(): ViewResolver {
-//        return ViewResolver  {_ ,  _,  _ ->  null}
-//
-//        }
-//    }
-//}
+package org.bdch.groovy
+
+
+class WebConfig {
+
+    init {
+    println(" Foo")
+    }
+}
