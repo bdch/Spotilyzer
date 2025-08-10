0a88ba0d769855a0a6d47eade12d5ccd074d751d
bdch <de.zac007@gmail.com>
2025-08-10 14:24:59 +0200

    - added  new GORM

---
+++ grails-app/domain/org/bdch/SpotifyTopTrack.groovy
@@ -0,0 +1,31 @@
+package org.bdch
+
+import java.sql.Timestamp
+
+class SpotifyTopTrack {
+
+   Long id
+   Long userSpotifyId
+   String trackId
+   String trackName
+   Integer popularity
+   Timestamp fetchedAt
+
+   String timeRange // e.g. "short_term", "medium_term", "long_term"
+   // NOTE:  "medium_term" is default according to Spotify API documentation:
+   // https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+
+   static mapping = {
+      table 'spotify_top_tracks'
+      id column: 'id'
+      userSpotifyId column: 'user_spotify_id'
+      trackId column: 'spotify_track_id'
+      trackName column: 'track_name'
+      popularity column: 'popularity'
+      fetchedAt column: 'fetched_at'
+      timeRange column: 'time_range'
+   }
+
+   static constraints = {
+   }
+}
